9cd4f4f40c73a73245825a431f1a78c32c58acea
will-leeson <wel2vw@virginia.edu>
2021-04-22 19:56:30 -0400

    Cuda working

--- src/networks/ggnn.py
+++ src/networks/ggnn.py
@@ -1,10 +1,11 @@
 import torch, copy, tqdm, time, copy
 import torch.nn as nn
 import torch.nn.functional as f
+import gc
 from scipy.stats import spearmanr
 
 class GGNN(nn.Module):
-    def __init__(self, passes, numEdgeSets, hiddenSize):
+    def __init__(self, passes, numEdgeSets):
         super(GGNN, self).__init__()
         self.passes = passes
         if self.passes > 0:
@@ -12,9 +13,9 @@ class GGNN(nn.Module):
             self.edgeNets = []
             for item in range(numEdgeSets):
                 self.edgeNets.append(nn.Linear(150,150))
-        self.fc1 = nn.Linear(151, hiddenSize)
-        self.fc2 = nn.Linear(hiddenSize, 10)
-        #self.fc3 = nn.Linear(20,10)
+        self.fc1 = nn.Linear(151, 80)
+        self.fc2 = nn.Linear(80, 10)
+        # self.fc3 = nn.Linear(20,10)
     
     
     def collect_incoming(self, node_spot, nodes, edges):
@@ -37,12 +38,11 @@ class GGNN(nn.Module):
             counter+=1
         return incoming
 
-    def forward(self, problemClass, nodesBatch, backwards_edge_dictBatch):
+    def forward(self, problemClass, nodesBatch, backwards_edge_dictBatch, cuda):
         """
         problemClass: A batch of tensors that represent what kind of verification problem the scores correspond to
         nodesBatch: A batch of sets of nodes. Each set represents a graph
         backwards_edge_dictBatch: A batch of dicts. Each key in the dict is a node, the value for each key is the nodes inset
-        incoming: A set of incoming representation (TODO: Refactor to remove)
         The forward function of the neural net. For each pass, perform the GGNN step: for each graph G, for each node N
         in G, collect the sum, S, of N's inset nodes. Pass N's representation with S. This is the new representation of N.
         Once all passes are complete, sum the nodes representations in a graph to form a graph feature vector. Take the
@@ -50,35 +50,31 @@ class GGNN(nn.Module):
         Pass the graph feature vectors through two linear layers with a tanh activation function in between and return
         the scores
         """
-        for _ in range(self.passes):   
-            nodesBatchCopy = copy.copy(nodesBatch)
+        for _ in range(self.passes):
+            inputsList = []
             for i in range(len(nodesBatch)):
                 inputs = []
                 edge_dict = backwards_edge_dictBatch[i]
                 for node_spot in range(len(nodesBatch[i])):
-                    input1 = self.collect_incoming(node_spot, nodesBatchCopy[i], edge_dict)
+                    input1 = self.collect_incoming(node_spot, nodesBatch[i], edge_dict)
                     inputs.append(input1)
-
                 inputs = torch.stack(inputs)
-                new_rep = self.gru(inputs, nodesBatch[i].float())
-                nodesBatch[i] = new_rep
+                inputsList.append(inputs.cuda())
+            phase1 = torch.cuda.memory_allocated()
+
+            for i in range(len(nodesBatch)):
+                nodesBatch[i] = f.relu(torch.nan_to_num(torch.log(self.gru(inputsList[i], nodesBatch[i].float()).sum(dim=0))))
                 #torch.cuda.empty_cache()
-   
-        for i in range(len(nodesBatch)):
-            #We use take the log so graph size isn't a factor
-            nodesBatch[i] = torch.log(nodesBatch[i].sum(dim=0))
-            nodesBatch[i][nodesBatch[i]!=nodesBatch[i]] = 0
-            nodesBatch[i] = f.relu(nodesBatch[i])
-            nodesBatch[i] = torch.cat((nodesBatch[i], problemClass[i]))
-            assert(len(nodesBatch[i])==151)
+            del inputsList
+
         x = torch.stack(nodesBatch)
+        x = torch.cat((x, problemClass), dim=1)
         x = self.fc1(x)
         x = f.leaky_relu(x) 
 #        x = torch.tanh(x)
         x = self.fc2(x)
         #x = f.relu(x)
         #x = self.fc3(x)
-
         return x
 
 def my_collate(batch):
@@ -95,7 +91,7 @@ def my_collate(batch):
 
     return ((tokens, problemClass, backwards_edge_dict), labels)
 
-def train_model(model, loss_fn, batchSize, trainset, valset, optimizer, scheduler, num_epochs):
+def train_model(model, loss_fn, batchSize, trainset, valset, optimizer, scheduler, num_epochs, cuda):
     start = time.time()
 
     train_loader = torch.utils.data.DataLoader(dataset=trainset, batch_size=batchSize, shuffle=True, collate_fn=my_collate)
@@ -112,27 +108,42 @@ def train_model(model, loss_fn, batchSize, trainset, valset, optimizer, schedule
 
         model.train()
         for (i, ((tokenSets, problemTypes, backwards_edge_dicts), labels)) in enumerate(tqdm.tqdm(train_loader)):
-            scores = model(problemTypes, tokenSets, backwards_edge_dicts)
             lossTensor = torch.FloatTensor([0])
+            if cuda:
+                for item in range(len(tokenSets)):
+                    tokenSets[item] = tokenSets[item].cuda()
+                problemTypes = problemTypes.cuda()
+                labels = labels.cuda()
+                lossTensor = lossTensor.cuda()
+                
+            scores = model(problemTypes, tokenSets, backwards_edge_dicts, cuda)
             loss = loss_fn(scores, labels, lossTensor)
             cum_loss+=loss.cpu().detach().item()
+            # above = 0
+            # below = 0
 
             for j in range(len(labels)):
                 corr, _ = spearmanr(labels[j].cpu().detach(), scores[j].cpu().detach().tolist())
                 corr_sums[problemTypes[j]]+=corr
                 counters[problemTypes[j]]+=1
                 corr_sum+=corr
+                # if corr > 0.8:
+                #     above+=1
+                # elif corr <0.2:
+                #     below +=1
                 assert(corr <=1)
             # maxScoresIdx = scores.argmax(dim=1).reshape(len(scores),1)
             # gather = labels.gather(1, maxScoresIdx)
             # corr_sum+=(gather>1).sum().item()
             
+            # print("Above 0.8:",above)
+            # print("Below 0.2:",below)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            # if (i+1) % 10 == 0 or (i+1)==len(train_loader):
-            #     print("Train-epoch", epoch, ". Iteration", i+1, "/", len(train_loader), ", Avg-Loss:", round(cum_loss/(i+1),4), "Avg-Corr:", round(corr_sum/((i+1)*batchSize),4))
+            if (i+1) % 100 == 0 or (i+1)==len(train_loader):
+                print("Train-epoch", epoch, ". Iteration", i+1, "/", len(train_loader), ", Avg-Loss:", round(cum_loss/(i+1),4), "Avg-Corr:", round(corr_sum/((i+1)*batchSize),4))
             #     if counters[0] > 0:
             #         print("No Overflow Avg-Corr:", round(corr_sums[0]/counters[0],4))
             #     if counters[1] > 0:
@@ -143,14 +154,20 @@ def train_model(model, loss_fn, batchSize, trainset, valset, optimizer, schedule
             #         print("Mem Valid Avg-Corr:", round(corr_sums[3]/counters[3],4))
             train_accuracies.append(round(corr_sum/((i+1)*batchSize),4))
             train_losses.append(round(cum_loss/(i+1),4))
+            del lossTensor
 
         corr_sum = 0.0
         cum_loss = 0.0
         model.eval()
 
         for (i, ((tokenSets, problemTypes, backwards_edge_dicts), labels)) in enumerate(tqdm.tqdm(val_loader)):
-            scores = model(problemTypes, tokenSets, backwards_edge_dicts)
-            lossTensor = torch.FloatTensor([0])
+            if cuda:
+                for item in range(len(tokenSets)):
+                    tokenSets[item] = tokenSets[item].cuda()
+                problemTypes = problemTypes.cuda()
+                labels = labels.cuda()
+            scores = model(problemTypes, tokenSets, backwards_edge_dicts, cuda)
+            lossTensor = torch.FloatTensor([0]).cuda()
             cum_loss+=loss_fn(scores, labels, lossTensor).cpu().detach().item()
 
             for j in range(len(labels)):
@@ -161,6 +178,7 @@ def train_model(model, loss_fn, batchSize, trainset, valset, optimizer, schedule
         scheduler.step(cum_loss/(i+1))
         if optimizer.param_groups[0]['lr']<1e-7:
             break
+        del lossTensor
 
         print("Validation-epoch", epoch, "Avg-Loss:", round(cum_loss/(i+1),4), ", Avg-Corr:", round(corr_sum/(len(valset)),4))
     
--- src/networks/ggnnTest.py
+++ src/networks/ggnnTest.py
@@ -1,5 +1,5 @@
 from ggnn import GGNN, train_model
-from utils.utils import GraphDataset, modified_margin_rank_loss
+from utils.utils import GraphDataset, modified_margin_rank_loss, modified_margin_rank_loss_cuda
 import torch, json, os, tqdm
 from torch.utils.data import DataLoader, random_split
 import torch.nn as nn
@@ -10,27 +10,32 @@ from matplotlib import pyplot as plt
 
 classes = ["No_Overflow", "UnReach", "Termination", "MemSafety"]
 
+cuda = True
 
-#for passes in range(1,2):
-for hiddenSize in range(14):
-    print("Hidden Size", (hiddenSize+1)*10)
-    passes = 0
-    resultFile = json.load(open("../../data/SV-CompResults.json"))
-    timeLabels = [((key, [x[1] for x in resultFile[key]])) for key in resultFile if os.path.isfile("../../data/final_graphs/" + key.split("|||")[0]+".npy")]
-    #        timeLabels = [((key, [x[1] for x in resultFile[key]])) for key in resultFile if os.path.isfile("../../data/final_graphs/" + key.split("|||")[0]+".npy") and key.split("|||")[1]==str(classType)]
-    data = GraphDataset(timeLabels, "../../data/final_graphs/")
+passes = 1
+resultFile = json.load(open("../../data/SV-CompResults.json"))
+timeLabels = [((key, [x[1] for x in resultFile[key]])) for key in resultFile if os.path.isfile("../../data/final_graphs/" + key.split("|||")[0]+".npy")]
+#        timeLabels = [((key, [x[1] for x in resultFile[key]])) for key in resultFile if os.path.isfile("../../data/final_graphs/" + key.split("|||")[0]+".npy") and key.split("|||")[1]==str(classType)]
+data = GraphDataset(timeLabels, "../../data/final_graphs/")
 
-    model = GGNN(passes=passes, numEdgeSets=3, hiddenSize=(hiddenSize+1)*10)
-    train_set, val_set = random_split(data, [round(data.__len__()*.8), round(data.__len__()*.2)])
+model = GGNN(passes=passes, numEdgeSets=3)
+if cuda:
+    model = model.cuda()
+train_set, val_set = random_split(data, [round(data.__len__()*.8), round(data.__len__()*.2)])
+loss_fn = None
+if cuda:
+    loss_fn = modified_margin_rank_loss_cuda
+else:
     loss_fn = modified_margin_rank_loss
-    optimizer = optim.Adam(model.parameters(), lr = 1e-3, weight_decay=1e-4)
-    epochs = 5
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=2, verbose=True)
-    train_acc, train_loss, val_acc, val_loss, time = train_model(model=model, loss_fn = loss_fn, batchSize=100, trainset=train_set, valset=val_set, optimizer=optimizer, scheduler=scheduler, num_epochs=epochs)
 
-    train_acc = np.array(train_acc)
-    train_loss = np.array(train_loss)
-    val_acc = np.array(val_acc)
-    val_loss = np.array(val_loss)
+optimizer = optim.Adam(model.parameters(), lr = 1e-3, weight_decay=1e-4)
+epochs = 5
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=2, verbose=True)
+train_acc, train_loss, val_acc, val_loss, time = train_model(model=model, loss_fn = loss_fn, batchSize=20, trainset=train_set, valset=val_set, optimizer=optimizer, scheduler=scheduler, num_epochs=epochs, cuda=cuda)
 
-    np.savez_compressed(str(passes)+"_passes_"+str(epochs)+"_epochs.npz", train_acc, train_loss, val_acc, val_loss)
+train_acc = np.array(train_acc)
+train_loss = np.array(train_loss)
+val_acc = np.array(val_acc)
+val_loss = np.array(val_loss)
+
+#np.savez_compressed(str(passes)+"_passes_"+str(epochs)+"_epochs.npz", train_acc, train_loss, val_acc, val_loss)
