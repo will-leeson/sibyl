4213b857fd8b44299d46666a2a25de9cfccf8b9f
will-leeson <wel2vw@virginia.edu>
2021-05-12 13:07:28 -0400

    Editing test script

--- src/networks/runTests.sh
+++ src/networks/runTests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-PASSES=0
 NUM=1111
-for i in {1..5}
+for PASSES in {0..5}
 do
-	sbatch --output="$PASSESpassesNoGRU$i.output" -p gpu --gres=gpu:1 ./netTrainer.sh $PASSES 100 1 1 $NUM
-	((NUM=NUM+1))
+	for i in {1..5}
+	do
+		sbatch --output="$PASSESpassesNoGRU$i.output" -p gpu --gres=gpu:1 ./netTrainer.sh $PASSES 100 1 1 $NUM
+		((NUM=NUM+1))
+	done
 done
