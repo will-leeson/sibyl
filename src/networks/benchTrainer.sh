650b2221c1c12dc263f02ab221e1732d36d481e3
will-leeson <wel2vw@virginia.edu>
2021-06-03 10:35:28 -0400

    Adding Benchexec Training

---
+++ src/networks/benchTrainer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export NUM_NODES=1
+export NUM_GPUS_PER_NODE=$4
+export NODE_RANK=0
+export WORLD_SIZE=$(($NUM_NODES * $NUM_GPUS_PER_NODE))
+
+python3 -m torch.distributed.launch \
+    --nproc_per_node=$NUM_GPUS_PER_NODE \
+    --nnodes=$NUM_NODES \
+    --node_rank $NODE_RANK \
+    --master_port=$5 \
+    pescoTrainer.py -t $1 -e $2 --architecture $3
