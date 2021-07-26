8547c611a170c8b4f855ce034564c5ddce830587
will-leeson <wel2vw@virginia.edu>
2021-07-26 11:01:54 -0400

    Convert Quals work to pytorch geometric

--- src/networks/runTests.sh
+++ src/networks/runTests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for comb in 'AST CFG DFG' 'AST CFG' 'AST DFG' 'CFG DFG' 'AST' 'CFG' 'DFG'
+for i in {0..9}
 do
-	for PASSES in {1..5}
+	for comb in 'AST CFG DFG' 'AST CFG' 'AST DFG' 'CFG DFG' 'AST' 'CFG' 'DFG'
 	do
-		for i in {0..9}
-		do
-			sbatch --exclude=ai[01-06] --output="${comb}${PASSES}passes$i.output" -p gpu --gres=gpu:1 ./netTrainer.sh $PASSES 50 3 overflow "$comb"
-		done
-	done	
+		for PASSES in {1..5}
+		do	
+			./netTrainer.sh $PASSES 50 "GAT" memSafety "$comb"	
+		done	
+	done
 done
