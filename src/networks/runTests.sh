cdb3fd092d641764ee53dad9e1dac3f1ac51dbf3
will-leeson <wel2vw@virginia.edu>
2021-07-30 10:04:21 -0400

    Edited Test harness

--- src/networks/runTests.sh
+++ src/networks/runTests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-for i in {0..9}
+for i in {0..2}
 do
 	for comb in 'AST CFG DFG' 'AST CFG' 'AST DFG' 'CFG DFG' 'AST' 'CFG' 'DFG'
 	do
-		for PASSES in {1..5}
-		do	
-			./netTrainer.sh $PASSES 50 "GAT" memSafety "$comb"	
+		for collate in 'sum' 'mean' 'max'
+		do
+			for PASSES in {1..5}
+			do	
+				sbatch --exclude=ai[01,04],affogato11,cheetah01,lynx[01,03-04,07] --output="${comb}${PASSES}Passes${collate}${i}.output" -p gpu --gres=gpu:1 ./netTrainer.sh $PASSES 50 "GAT" overflow "$comb" "$collate"	
+			done
 		done	
 	done
 done
