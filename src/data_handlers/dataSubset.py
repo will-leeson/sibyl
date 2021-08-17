9933ca8927dd7dddc568fbf5959bbe43368f4cd0
will-leeson <wel2vw@virginia.edu>
2021-08-17 12:22:40 -0400

    DataSubset script

--- src/data_handlers/dataSubset.py
+++ src/data_handlers/dataSubset.py
@@ -8,7 +8,10 @@ resultFile = json.load(open("../../data/SV-CompResults.json"))
 files = [[],[],[],[]]
 for key in tqdm.tqdm(resultFile):
     if os.path.isfile("../../data/final_graphs/" + key.split("|||")[0]+".npz"):
-        files[int(key.split("|||")[1])].append((key, resultFile[key]))
+        a = np.load("../../data/final_graphs/" + key.split("|||")[0]+".npz")['node_rep']
+        if a.size >40000:
+            files[int(key.split("|||")[1])].append((key, resultFile[key]))
+
 
 trainFiles, valFiles, testFiles = [], [], []
 for i in files:
@@ -23,6 +26,7 @@ testFileDict = dict()
 
 for item in trainFiles:
     trainFileDict[item[0]] = item[1]
+print(len(trainFileDict))
 
 json.dump(trainFileDict, open("../../data/subsetTrainFiles.json",'w'))
 
