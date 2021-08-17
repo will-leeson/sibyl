8f453d12d4aede04bbf6ff45dd6fce84566db43d
will-leeson <wel2vw@virginia.edu>
2021-08-17 12:04:30 -0400

    DataSubset script

---
+++ src/data_handlers/dataSubset.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+import os, json, torch, tqdm, numpy as np
+
+resultFile = json.load(open("../../data/SV-CompResults.json"))
+
+files = [[],[],[],[]]
+for key in tqdm.tqdm(resultFile):
+    if os.path.isfile("../../data/final_graphs/" + key.split("|||")[0]+".npz"):
+        files[int(key.split("|||")[1])].append((key, resultFile[key]))
+
+trainFiles, valFiles, testFiles = [], [], []
+for i in files:
+    x, y, z= torch.utils.data.random_split(i, [len(i) - (len(i)//10) - (len(i)//10),len(i)//10, len(i)//10])
+    trainFiles+=x
+    valFiles+=y
+    testFiles+=z
+
+trainFileDict = dict()
+valFileDict = dict()
+testFileDict = dict()
+
+for item in trainFiles:
+    trainFileDict[item[0]] = item[1]
+
+json.dump(trainFileDict, open("../../data/subsetTrainFiles.json",'w'))
+
+for item in valFiles:
+    valFileDict[item[0]] = item[1]
+json.dump(valFileDict, open("../../data/subsetValFiles.json",'w'))
+
+for item in testFiles:
+    testFileDict[item[0]] = item[1]
+json.dump(testFileDict, open("../../data/subsetTestFiles.json",'w'))
