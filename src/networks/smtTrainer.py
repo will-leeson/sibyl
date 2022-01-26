bd8cdd5d7e89d744e6f0b56c4527a05ae70908fb
will-leeson <wel2vw@virginia.edu>
2022-01-26 14:00:40 -0500

    Adding Uber Nodes for symbols

--- src/networks/smtTrainer.py
+++ src/networks/smtTrainer.py
@@ -1,10 +1,8 @@
-from gnn import GGNN, GAT
+from gnn import GAT
 from utils.utils import ModifiedMarginRankingLoss, train_model, evaluate, SMTDataset
 import torch, json, time, argparse
 import torch.optim as optim
 import numpy as np
-from torch_geometric.nn import GNNExplainer
-import torch_geometric
 
 '''
 File - netTrainer.py
@@ -17,7 +15,6 @@ if __name__ == '__main__':
 	parser.add_argument("-t", "--time-steps", help="Number of timesteps (Default=0)", default=0, type=int)
 	parser.add_argument("-e", "--epochs", help="Number of training epochs (Default=20)", default=20, type=int)
 	parser.add_argument("--edge-sets", help="Which edges sets to include: AST, Data(Default=All)", nargs='+', default=['AST', 'Data'], choices=['AST', 'Data'])
-	parser.add_argument('-n','--net', help="GGNN, GAT", default="GAT", choices=["GGNN","GAT"])
 	parser.add_argument("-m", "--mode", help="Mode for jumping (Default LSTM): max, cat, lstm", default="cat", choices=['max', 'cat', 'lstm'])
 	parser.add_argument("--pool-type", help="How to pool Nodes (max, mean, add, sort, attention, multiset)", default="mean", choices=["max", "mean","add","sort","attention","multiset"])
 	parser.add_argument("-g", "--gpu", help="Which GPU should the model be on", default=0, type=int)
@@ -28,15 +25,15 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	trainFiles = json.load(open("../../data/smtTrainFiles.json"))
-	trainLabels = [(key, [trainFiles[key][item][0] for item in trainFiles[key]]) for key in trainFiles]
+	trainLabels = [(key, [item[0] for item in trainFiles[key]]) for key in trainFiles]
 	
 	valFiles = json.load(open("../../data/smtValFiles.json"))
-	valLabels = [(key, [valFiles[key][item][0] for item in valFiles[key]]) for key in valFiles]
+	valLabels = [(key, [item[0] for item in valFiles[key]]) for key in valFiles]
 	
 	testFiles = json.load(open("../../data/smtTestFiles.json"))
-	testLabels = [(key, [testFiles[key][item][0] for item in testFiles[key]]) for key in testFiles ]
+	testLabels = [(key, [item[0] for item in testFiles[key]]) for key in testFiles]
 
-	dataLoc = "../../data/smtWUber/"
+	dataLoc = "../../data/smtFiles/"
 
 	train_set = SMTDataset(trainLabels, dataLoc, args.edge_sets, args.cache)
 	val_set = SMTDataset(valLabels, dataLoc, args.edge_sets, args.cache)
@@ -44,10 +41,7 @@ if __name__ == '__main__':
 
 	#getWeights(trainLabels)
 
-	if args.net == 'GGNN':
-		model = GGNN(passes=args.time_steps, numEdgeSets=len(args.edge_sets), inputLayerSize=train_set[0][0].x.size(1), outputLayerSize=len(trainLabels[0][1]), mode=args.mode).to(device=args.gpu)
-	else:
-		model = GAT(passes=args.time_steps, numEdgeSets=len(args.edge_sets), numAttentionLayers=5, inputLayerSize=train_set[0][0].x.size(1), outputLayerSize=len(trainLabels[0][1]), mode=args.mode, k=20, shouldJump=args.no_jump, pool=args.pool_type).to(device=args.gpu)
+	model = GAT(passes=args.time_steps, numEdgeSets=len(args.edge_sets), numAttentionLayers=5, inputLayerSize=train_set[0][0].x.size(1), outputLayerSize=len(trainLabels[0][1]), mode=args.mode, k=20, shouldJump=args.no_jump, pool=args.pool_type).to(device=args.gpu)
 
 	loss_fn = ModifiedMarginRankingLoss(margin=0.1, gpu=args.gpu).to(device=args.gpu)
 
