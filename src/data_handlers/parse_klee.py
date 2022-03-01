9cba919e264dc1b1c25b7b4e4d1e20e690dc5b38
will-leeson <wel2vw@virginia.edu>
2022-03-01 10:58:17 -0500

    Adding klee parsing

--- src/data_handlers/parse_klee.py
+++ src/data_handlers/parse_klee.py
@@ -1,15 +1,21 @@
 import os
+import glob
 
-smtFile = open("/home/wel2vw/Research/coreutils/obj-llvm/src/od-results/solver-queries.smt2")
+dirs = glob.glob("/p/graves/data/SMTKlee/*")
 
-# os.mkdir("../../data/kleeSMT/od")
+for dir in dirs:
+	program = dir.split("/")[-1]
+	if not os.path.exists("../../data/SMTKlee/"+program):
+		os.mkdir("../../data/SMTKlee/"+program)
+	
+	smtFile = open(dir+"solver-queries.smt2")
 
-counter = 0
-aFile = open("../../data/kleeSMT/od/od-%06d.smt2" % counter,'w')
-for line in smtFile:
-    if line == "\n":
-        aFile.close()
-        counter+=1
-        aFile = open("../../data/kleeSMT/od/od-%06d.smt2" % counter,'w')
-    else:
-        aFile.write(line)
+	counter = 0
+	aFile = open("../../data/kleeSMT/"+program+"/%06d.smt2" % counter,'w')
+	for line in smtFile:
+		if line == "\n":
+			aFile.close()
+			counter+=1
+			aFile = open("../../data/kleeSMT/"+program+"/%06d.smt2" % counter,'w')
+		else:
+			aFile.write(line)
