850817fc9429f10fb0fbcb7cdc53e026c5173735
will-leeson <wel2vw@virginia.edu>
2022-04-19 11:46:08 -0400

    Updating klee parser

--- src/data_handlers/parse_klee.py
+++ src/data_handlers/parse_klee.py
@@ -1,21 +1,29 @@
 import os
 import glob
+import sys
 
-dirs = glob.glob("/p/graves/data/SMTKlee/*")
+afile = sys.argv[1]
 
-for dir in dirs:
-	program = dir.split("/")[-1]
-	if not os.path.exists("../../data/SMTKlee/"+program):
-		os.mkdir("../../data/SMTKlee/"+program)
-	
-	smtFile = open(dir+"solver-queries.smt2")
+print(afile)
 
-	counter = 0
-	aFile = open("../../data/kleeSMT/"+program+"/%06d.smt2" % counter,'w')
-	for line in smtFile:
-		if line == "\n":
-			aFile.close()
-			counter+=1
-			aFile = open("../../data/kleeSMT/"+program+"/%06d.smt2" % counter,'w')
-		else:
-			aFile.write(line)
+program = os.path.basename(afile).split("-")[0]
+#program = "find"
+print(program)
+
+if not os.path.exists("../../data/SMTKlee/"+program):
+	os.mkdir("../../data/SMTKlee/"+program)
+
+smtFile = open(afile)
+
+counter = 0
+aFile = open("../../data/SMTKlee/"+program+"/%09d.smt2" % counter,'w')
+for line in smtFile:
+	if line == "\n":
+		aFile.close()
+		counter+=1
+	else:
+		if aFile.closed:
+			aFile = open("../../data/SMTKlee/"+program+"/%09d.smt2" % counter,'w')
+		aFile.write(line)
+
+print(counter)
